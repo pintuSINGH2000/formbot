a8882cdba1a39ba80c074cd9ab4eeca64dec1af5
pintuSINGH2000 <94526600+pintuSINGH2000@users.noreply.github.com>
2024-07-31 17:22:47 +0530

    formbot frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Loginpage from "./Pages/Loginpage";
+import Registerpage from "./Pages/Registerpage";
+import WorkspacePage from "./Pages/WorkspacePage";
+import FormPage from "./Pages/FormPage";
+import FormbotPage from "./Pages/FormbotPage";
+import SettingPage from "./Pages/SettingPage";
+import ProtectedRoute from "./Component/ProtectedRoute";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route exact path="/" element={<Homepage />}></Route>
+        <Route
+          path="/register"
+          element={
+            <ProtectedRoute Component={Registerpage} isAuthenticate={true} />
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={
+            <ProtectedRoute Component={Loginpage} isAuthenticate={true} />
+          }
+        ></Route>
+        <Route
+          path="/workspace/:folderId?"
+          element={<ProtectedRoute Component={WorkspacePage} />}
+        ></Route>
+        <Route
+          path="/workspace/:folderId?/form/:formId?"
+          element={<ProtectedRoute Component={FormPage} />}
+        ></Route>
+        <Route path="/form/:formId" element={<FormbotPage />}></Route>
+
+        <Route
+          path="/setting"
+          element={<ProtectedRoute Component={SettingPage} />}
+        ></Route>
+      </Routes>
+    </>
   );
 }
 
